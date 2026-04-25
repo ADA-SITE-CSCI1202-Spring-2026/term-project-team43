d8b893fdf34c77e6fd5a45a2a5f6fc0c8e4deb5b
MahammadGurbanov <mgurbanov23934@ada.edu.az>
2026-04-25 21:42:42 +0400

    Add centered lsit

--- src/frontend/OrderPanel.java
+++ src/frontend/OrderPanel.java
@@ -1,11 +1,15 @@
 package frontend;
 
+import backend.order.Order;
 import javax.swing.*;
 import java.awt.*;
 
 public class OrderPanel extends JPanel {
+    private static final DefaultListModel<Order> model = new DefaultListModel<>();
+
     public OrderPanel(){
         int TITLE_FONT_SIZE = 30;
+
         this.setBackground(new Color(0xF6B1B0));
         this.setLayout(new BorderLayout());
         JLabel title = new JLabel("Orders");
@@ -15,6 +19,36 @@ public class OrderPanel extends JPanel {
         titleWrapper.setOpaque(false);
         titleWrapper.add(title);
         title.setAlignmentX(JLabel.CENTER);
+
+        // Layout, for the BorderLayout.Center
+        JPanel centerPane = new JPanel();
+        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
+        centerPane.setOpaque(false);
+
+        // Create scrollable list
+        JList<Order> visualList = new JList<>(model);
+        visualList.setLayoutOrientation(JList.VERTICAL);
+        visualList.setFixedCellHeight(25);
+        visualList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        visualList.setBackground(new Color(0xAEEEEE));
+
+        JScrollPane scrollPane = new JScrollPane(visualList);
+        scrollPane.setPreferredSize(new Dimension(300, 180));
+        scrollPane.setMaximumSize(new Dimension(350, 200));
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.black, 5));
+
+        // List container
+        JPanel listPane = new JPanel();
+        listPane.setOpaque(false);
+        listPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+        listPane.add(scrollPane);
+
+        centerPane.add(Box.createVerticalGlue());
+        centerPane.add(Box.createRigidArea(new Dimension(0, 50)));
+        centerPane.add(listPane);
+        centerPane.add(Box.createVerticalGlue());
+
         this.add(titleWrapper, BorderLayout.NORTH);
+        this.add(centerPane, BorderLayout.CENTER);
     }
 }
