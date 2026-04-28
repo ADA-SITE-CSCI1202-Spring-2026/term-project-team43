227e2a69b9b9276af2efca78d76ff851164a7c1b
MahammadGurbanov <mgurbanov23934@ada.edu.az>
2026-04-28 22:42:16 +0400

    Create the Inventory panel

--- src/frontend/InventoryPanel.java
+++ src/frontend/InventoryPanel.java
@@ -4,8 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InventoryPanel extends JPanel {
+    private final static JTextArea inventoryInfo = new JTextArea(5, 25);
+
     public InventoryPanel(){
         int TITLE_FONT_SIZE = 30;
+        int TEXT_AREA_FONT_SIZE = 15;
+
         this.setBackground(new Color(0xF6B1B0));
         this.setLayout(new BorderLayout());
         JLabel title = new JLabel("Inventory");
@@ -15,6 +19,48 @@ public class InventoryPanel extends JPanel {
         titleWrapper.setOpaque(false);
         titleWrapper.add(title);
         title.setAlignmentX(JLabel.CENTER);
+
+        // Setting up TextArea used of inventoryInfo
+        inventoryInfo.setFont(
+                new Font(Font.SANS_SERIF, Font.ITALIC, TEXT_AREA_FONT_SIZE)
+        );
+
+        inventoryInfo.setEditable(false);
+        inventoryInfo.setLineWrap(true);
+        inventoryInfo.setWrapStyleWord(true);
+        inventoryInfo.setBackground(new Color(0xAEEEEE));
+
+        JScrollPane inventoryScrollPane = new JScrollPane(inventoryInfo);
+        inventoryScrollPane.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+        );
+
+        inventoryScrollPane.setPreferredSize(new Dimension(300, 250));
+        inventoryScrollPane.setMaximumSize(new Dimension(300, 270));
+        inventoryScrollPane.setBorder(
+                BorderFactory.createLineBorder(Color.black, 5)
+        );
+
+        // Displaying inventory Info
+        JPanel centerPane = new JPanel();
+        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
+        centerPane.setOpaque(false);
+
+        JPanel inventoryPane = new JPanel();
+        inventoryPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+        inventoryPane.setOpaque(false);
+        inventoryPane.add(inventoryScrollPane);
+
+        centerPane.add(Box.createVerticalGlue());
+        centerPane.add(Box.createRigidArea(new Dimension(0, 50)));
+        centerPane.add(inventoryPane);
+        centerPane.add(Box.createVerticalGlue());
+
         this.add(titleWrapper, BorderLayout.NORTH);
+        this.add(centerPane, BorderLayout.CENTER);
+    }
+
+    public static void setInventoryInfo(String info){
+        inventoryInfo.setText(info);
     }
 }
