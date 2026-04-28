ee121966cf9c3132f6bf546ebbee7300eeb20eb4
MahammadGurbanov <mgurbanov23934@ada.edu.az>
2026-04-28 22:47:13 +0400

    Add a dropdown list and label

--- src/frontend/RestockPanel.java
+++ src/frontend/RestockPanel.java
@@ -1,12 +1,21 @@
 package frontend;
 
+import backend.pantry.Ingredient;
 import javax.swing.*;
 import java.awt.*;
 
 public class RestockPanel extends JPanel {
+
+    private final static DefaultComboBoxModel<Ingredient> ingredientData =
+            new DefaultComboBoxModel<>();
+
     public RestockPanel(){
         int TITLE_FONT_SIZE = 30;
-        this.setBackground(new Color(0xF6B1B0));
+        int PINK_BACKGROUND = 0xF6B1B0;
+        int CYAN_BACKGROUND = 0xAEEEEE;
+        int DROPDOWN_LABEL_FONT_SIZE = 15;
+
+        this.setBackground(new Color(PINK_BACKGROUND));
         this.setLayout(new BorderLayout());
         JLabel title = new JLabel("Restock");
         title.setFont(new Font(Font.SANS_SERIF, Font.BOLD, TITLE_FONT_SIZE));
@@ -15,6 +24,35 @@ public class RestockPanel extends JPanel {
         titleWrapper.setOpaque(false);
         titleWrapper.add(title);
         title.setAlignmentX(JLabel.CENTER);
+
+        // Layout, for the BorderLayout.Center
+        JPanel centerPane = new JPanel();
+        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
+        centerPane.setOpaque(false);
+
+        // Dropdown List
+        JLabel listLabel = new JLabel("Selected Ingredient: ");
+        listLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, DROPDOWN_LABEL_FONT_SIZE));
+
+        JComboBox<Ingredient> ingredientList = new JComboBox<>(ingredientData);
+        ingredientList.setBackground(new Color(CYAN_BACKGROUND));
+        ingredientList.setPreferredSize(new Dimension(300, 50));
+        ingredientList.setMaximumSize(new Dimension(350, 80));
+
+        // Dropdown List container
+        JPanel dropdownPane = new JPanel();
+        dropdownPane.setOpaque(false);
+        dropdownPane.setLayout(new BoxLayout(dropdownPane, BoxLayout.X_AXIS));
+        dropdownPane.add(listLabel);
+        dropdownPane.add(Box.createRigidArea(new Dimension(-3, 0)));
+        dropdownPane.add(ingredientList);
+
+        centerPane.add(Box.createVerticalGlue());
+        centerPane.add(dropdownPane);
+        centerPane.add(Box.createVerticalGlue());
+        centerPane.add(Box.createVerticalGlue());
+
+        this.add(centerPane, BorderLayout.CENTER);
         this.add(titleWrapper, BorderLayout.NORTH);
     }
 }
