5f4d65efc18f2bdc29e8dea38cfde6eb70ab07e5
ElvinGarabayli <egarabayli23202@ada.edu.az>
2026-05-04 21:37:18 +0400

    Add hasIce property to Beverage class

--- src/backend/menu/Beverage.java
+++ src/backend/menu/Beverage.java
@@ -1,4 +1,36 @@
 package backend.menu;
 
+import backend.pantry.Ingredient;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 public class Beverage extends MenuItem{
+    private boolean hasIce;
+
+    public Beverage(String name, BigDecimal price,
+                    HashMap<Ingredient, Integer> ingredientCounts,
+                    boolean hasIce
+    ){
+        super(name, price, ingredientCounts);
+        this.hasIce = hasIce;
+    }
+
+    public boolean getHasIce(){
+        return hasIce;
+    }
+
+    public void setHasIce(boolean hasIce){
+        this.hasIce = hasIce;
+    }
+
+    @Override
+    public String toString(){
+        if (hasIce){
+            return super.toString() + " with ice";
+        } else {
+            return super.toString();
+        }
+    }
+
 }
