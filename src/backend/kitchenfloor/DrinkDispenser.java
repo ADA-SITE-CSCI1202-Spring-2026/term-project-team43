790997200b08c7edf466e76f9bc0ac9a094f6ac8
AytajJafarli <ajafarli23275@ada.edu.az>
2026-05-04 21:17:18 +0400

    Update DrinkDispenser.java

--- src/backend/kitchenfloor/DrinkDispenser.java
+++ src/backend/kitchenfloor/DrinkDispenser.java
@@ -1,4 +1,9 @@
 package backend.kitchenfloor;
 
+import backend.menu.MenuItem;
+
 public class DrinkDispenser implements IAppliance{
+    public String process(MenuItem item){
+        return item.getName() + " is being dispensed from the drink dispenser";
+    }
 }
