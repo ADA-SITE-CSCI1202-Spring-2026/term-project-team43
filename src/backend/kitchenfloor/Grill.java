704124bd07fa6fd092f45b0791ab5120bfcbff1b
AytajJafarli <ajafarli23275@ada.edu.az>
2026-05-04 21:19:03 +0400

    Update Grill.java

--- src/backend/kitchenfloor/Grill.java
+++ src/backend/kitchenfloor/Grill.java
@@ -1,4 +1,11 @@
 package backend.kitchenfloor;
 
+import backend.menu.MenuItem;
+
 public class Grill implements IAppliance{
+    @Override
+    public String process(MenuItem item){
+        return item.getName() + " is being prepared on the grill";
+    }
+
 }
